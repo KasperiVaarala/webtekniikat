c6a5a08ae50d395fef6bddc3c3550c1c28205c10
Kasperi Vaarala <n1vaka00@students.oamk.fi>
2021-11-22 11:07:36 +0200

    tehtävii

--- 15.11.2021/code.js
+++ 15.11.2021/code.js
@@ -1,22 +1,34 @@
-document.querySelector("button").addEventListener("click", addNote)
+
+//Lisätään napin kuuntelijafunktio
+let form = document.querySelector("form")
+form.addEventListener("submit", addNote)
 let body = document.querySelector("body")
-let n = document.getElementById("name")
-let note = document.getElementById("note")
-let checkbox = document.getElementById("important")
 
-function addNote() {
+
+function addNote(event){
+    event.preventDefault()
+
+    formData = new FormData(form)
+
+    //Luodaan otsikko johon tulee nimi-kentästä nimi ja lisäksi aikaleima
     let h3 = document.createElement("h3")
-    h3.textContent = n.value + " " + new Date().toLocaleString()
-    body.appendChild(h3)
-    let p = document.createElement("p")
-    p.textContent = note.value;
+    h3.textContent = formData.get("name") + "  " + new Date().toLocaleString();
 
-    if (checkbox.checked){
-       h3.classList.add("important") 
-       p.classList.add("important")
+    //Itse note lisätään tekstinä p-elementtiin textareasta
+    let p = document.createElement("p")
+    p.textContent = formData.get("note")
+
+    console.log(formData.get("important"));
+
+    //Jos checkbox valittu, lisätään important muotoilu
+    if(formData.get("important")){
+        h3.classList.add("important")
+        p.classList.add("important")
     }
 
-
-    body.append(h3, p, document.createElement("hr"))
+    //Kaikki elementit uusina bodyn sisälle
+    body.append(h3,p, document.createElement("hr"))
 
 }
+
+
